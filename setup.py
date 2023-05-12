76bfb0ed25623baf054dac5c80235381ad5cafa4
clustercosmos <p_rooney@hotmail.com>
2023-05-12 17:36:37 +0200

    sorted directory structure
    moved things into better place

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="xdp",
+    version="0.1.0",
+    description="A package to process x-ray astrophysical data",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "astroquery",
+        "matplotlib",
+        "seaborn",
+        "tarfile",
+        "subprocess"
+    ],
+    author="Philip Rooney",
+    author_email="p_rooney@hotmail.com",
+    license="MIT",
+    url="https://github.com/clustercosmos/xdp",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
